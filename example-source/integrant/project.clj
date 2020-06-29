407ad76e301a915bcad4cc8576ae78d01e7394ba
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2020-06-28 20:15:06 -0400

    some more example code

--- example-source/integrant/project.clj
+++ example-source/integrant/project.clj
@@ -4,4 +4,5 @@
 
   :dependencies [[org.clojure/clojure   "1.10.0" :scope "provided"]
                  [integrant             "0.8.0"]
-                 [duct/core             "0.8.0"]])
+                 [duct/core             "0.8.0"]
+                 [sweet-tooth/sweet-tooth-endpoint "0.10.4"]])
